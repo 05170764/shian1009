9f2bfb07597c9bbcc0b13f6bb63f8caf40a7a086
05170764 <62740965+05170764@users.noreply.github.com>
2020-05-08 15:45:25 +0800

    Update func.py

--- module/func.py
+++ module/func.py
@@ -97,7 +97,7 @@ def sendButton(event):  #按鈕樣版
         message = TemplateSendMessage(
             alt_text='按鈕樣板',
             template=ButtonsTemplate(
-                thumbnail_image_url='https://reurl.cc/arZqOD',  #顯示的圖片
+                thumbnail_image_url='https://reurl.cc/3D6XvR',  #顯示的圖片
                 title='蛋糕好好吃',  #主標題
                 text='請選擇：',  #副標題
                 actions=[
